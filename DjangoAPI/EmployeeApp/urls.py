ac2d1555ab022cf51aebf3c3e4c902f4fc16765e
Wan Zulmuhammad Harith <wzharith@gmail.com>
2021-07-21 11:46:24 +0800

    add employee api and urls

--- DjangoAPI/EmployeeApp/urls.py
+++ DjangoAPI/EmployeeApp/urls.py
@@ -4,4 +4,7 @@ from EmployeeApp import views
 urlpatterns=[
     url(r'^department/$', views.departmentApi),
     url(r'^department/([0-9]+)$', views.departmentApi),
+
+    url(r'^employee/$', views.employeeApi),
+    url(r'^employee/([0-9]+)$', views.employeeApi),
 ]
--- DjangoAPI/EmployeeApp/views.py
+++ DjangoAPI/EmployeeApp/views.py
@@ -36,3 +36,32 @@ def departmentApi(request,id=0):
         department=Department.objects.get(DepartmentId=id)
         department.delete()
         return JsonResponse("Deleted Succeffully!!", safe=False)
+
+@csrf_exempt
+def employeeApi(request,id=0):
+    if request.method=='GET':
+        employees = Employee.objects.all()
+        employees_serializer = EmployeeSerializer(employees, many=True)
+        return JsonResponse(employees_serializer.data, safe=False)
+
+    elif request.method=='POST':
+        employee_data=JSONParser().parse(request)
+        employee_serializer = EmployeeSerializer(data=employee_data)
+        if employee_serializer.is_valid():
+            employee_serializer.save()
+            return JsonResponse("Added Successfully!!" , safe=False)
+        return JsonResponse("Failed to Add.",safe=False)
+    
+    elif request.method=='PUT':
+        employee_data = JSONParser().parse(request)
+        employee=Employee.objects.get(EmployeeId=employee_data['EmployeeId'])
+        employee_serializer=EmployeeSerializer(employee,data=employee_data)
+        if employee_serializer.is_valid():
+            employee_serializer.save()
+            return JsonResponse("Updated Successfully!!", safe=False)
+        return JsonResponse("Failed to Update.", safe=False)
+
+    elif request.method=='DELETE':
+        employee=Employee.objects.get(EmployeeId=id)
+        employee.delete()
+        return JsonResponse("Deleted Succeffully!!", safe=False)
