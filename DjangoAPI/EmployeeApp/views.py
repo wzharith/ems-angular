6470b85c861be0b52dd50f225f596b2b23dfbb38
Wan Zulmuhammad Harith <wzharith@gmail.com>
2021-07-21 11:25:27 +0800

    add views for department

--- DjangoAPI/EmployeeApp/views.py
+++ DjangoAPI/EmployeeApp/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import JsonResponse
+
+from rest_framework.parsers import JSONParser
+
+from .models import Department, Employee
+from .serializers import DepartmentSerializer, EmployeeSerializer
 
 # Create your views here.
+@csrf_exempt
+def departmentApi(request,id=0):
+    if request.method=='GET':
+        departments = Department.objects.all()
+        departments_serializer = DepartmentSerializer(departments, many=True)
+        return JsonResponse(departments_serializer.data, safe=False)
+
+    elif request.method=='POST':
+        department_data=JSONParser().parse(request)
+        department_serializer = DepartmentSerializer(data=department_data)
+        if department_serializer.is_valid():
+            department_serializer.save()
+            return JsonResponse("Added Successfully!!" , safe=False)
+        return JsonResponse("Failed to Add.",safe=False)
+    
+    elif request.method=='PUT':
+        department_data = JSONParser().parse(request)
+        department=Department.objects.get(DepartmentId=department_data['DepartmentId'])
+        department_serializer=DepartmentSerializer(department,data=department_data)
+        if department_serializer.is_valid():
+            department_serializer.save()
+            return JsonResponse("Updated Successfully!!", safe=False)
+        return JsonResponse("Failed to Update.", safe=False)
+
+    elif request.method=='DELETE':
+        department=Department.objects.get(DepartmentId=id)
+        department.delete()
+        return JsonResponse("Deleted Succeffully!!", safe=False)
