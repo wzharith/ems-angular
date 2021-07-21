3ad038ffbac6f5bbcbfa00d65e5ad22b69de3755
Wan Zulmuhammad Harith <wzharith@gmail.com>
2021-07-21 12:01:37 +0800

    add save file url

--- DjangoAPI/EmployeeApp/urls.py
+++ DjangoAPI/EmployeeApp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
 
-from EmployeeApp import views
+from . import views
 
 urlpatterns=[
     url(r'^department/$', views.departmentApi),
