cb2ef2aaaeef9d259e5e2471852f9c0a28faa5ee
Wan Zulmuhammad Harith <wzharith@gmail.com>
2021-07-21 12:32:53 +0800

    add components and shared service

--- FrontUI/src/app/app.module.ts
+++ FrontUI/src/app/app.module.ts
@@ -3,16 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDepComponent } from './department/show-dep/show-dep.component';
+import { EditDepComponent } from './department/edit-dep/edit-dep.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmpComponent } from './department/show-emp/show-emp.component';
+import { EditEmpComponent } from './department/edit-emp/edit-emp.component';
+import { SharedService } from './shared.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    ShowDepComponent,
+    EditDepComponent,
+    EmployeeComponent,
+    ShowEmpComponent,
+    EditEmpComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ FrontUI/src/app/department/edit-dep/edit-dep.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-dep',
+  templateUrl: './edit-dep.component.html',
+  styleUrls: ['./edit-dep.component.css']
+})
+export class EditDepComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
